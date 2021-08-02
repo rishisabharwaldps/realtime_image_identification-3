b72a9876697669fcb1b8869c1425b293d6d9c803
rishisabharwaldps <75731259+rishisabharwaldps@users.noreply.github.com>
2021-08-02 17:36:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(250, 250);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/TrGTRUPDS/model.json',modelLoaded);
+}
+
+function modelLoaded(){
+    console.log('Model Laoded');
+}
+function draw(){
+    image(video, 0, 0, 250, 250);
+    classifier.classify(video, gotResult);
+} 
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
